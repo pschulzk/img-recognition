53e72b93f6f815af8770023a9dad3ad06e817ec3
Philip Schulz-Klingauf <psk@schulz-klingauf.de>
2023-07-08 14:43:33 +0200

    feat: BE use fastify and implement Swagger

--- apps/fbn-be/src/main.ts
+++ apps/fbn-be/src/main.ts
@@ -5,11 +5,31 @@
 
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { version } from '../../../package.json'
 
 import { AppModule } from './app/app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
+  app.enableShutdownHooks()
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Fastify Streaming Server')
+    // .setDescription('Stream files to and from a MongoDB.')
+    .setVersion(version)
+    .addTag('File')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api/docs', app, document)
+
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
   const port = process.env.PORT || 3000
